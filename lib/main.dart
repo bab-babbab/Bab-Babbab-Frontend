e9365ddb83e3d813c5bdbf50180b9ad8543df814
beebe0mg <w2321@e-mirim.hs.kr>
2025-06-04 00:26:45 +0900

    ✨ [chore] : 카메라 기능 접근

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,8 @@ import 'package:bab_babbab_front/screens/main/InfoPage.dart';
 import 'package:flutter/material.dart';
 import 'package:bab_babbab_front/firebase_options.dart';
 import 'package:firebase_core/firebase_core.dart';
-
+import 'package:bab_babbab_front/screens/posts/posts_page.dart';
+import 'package:bab_babbab_front/screens/posts/post_detail.dart';
 import 'package:bab_babbab_front/screens/home/home.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 
@@ -32,7 +33,7 @@ class MainApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'bab-babbab',
 
-      home: InformationPage(),
+      home: PostsPage(),
     );
   }
 }
